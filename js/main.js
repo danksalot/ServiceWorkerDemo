970c95ae23d78b23dd50cd5c5578ad4bbcb7a17c
Jacob Danks <jacob.danks@inflection.com>
2021-10-12 23:45:54 -0600

    Small updates

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ if ('serviceWorker' in navigator) {
 	});
 }
 
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
 function displayNotification() {
 	if (Notification.permission == 'granted') {
 		navigator.serviceWorker.getRegistration().then(function(reg) {
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'cache-v23';
+const CACHE_NAME = 'cache-v24';
 const urlsToCache = [
 	// STATIC PAGES
 	// '/',
@@ -91,4 +91,25 @@ self.addEventListener('notificationclick', function(e) {
 		clients.openWindow('http://www.example.com');
 		notification.close();
 	}
+});
+
+self.addEventListener('push', function(e) {
+	var options = {
+		body: 'This notification was generated from a push!',
+		icon: 'images/example.png',
+		vibrate: [100, 50, 100],
+		data: {
+			dateOfArrival: Date.now(),
+			primaryKey: '2'
+		},
+		actions: [
+			{action: 'explore', title: 'Explore this new world',
+				icon: 'images/checkmark.png'},
+			{action: 'close', title: 'Close',
+				icon: 'images/xmark.png'},
+		]
+	};
+	e.waitUntil(
+		self.registration.showNotification('Hello world!', options)
+	);
 });
